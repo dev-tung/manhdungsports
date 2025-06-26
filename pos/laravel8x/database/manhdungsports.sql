e0a9617a2ec71b3560d2687e76ee06b3529db2bd
Tung <devtung27101992@gmail.com>
2025-06-26 10:52:58 +0700

    update code

--- pos/laravel8x/database/manhdungsports.sql
+++ pos/laravel8x/database/manhdungsports.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 25/06/2025 17:11:02
+ Date: 26/06/2025 10:52:41
 */
 
 SET NAMES utf8mb4;
@@ -29,23 +29,27 @@ CREATE TABLE `customer`  (
   `customer_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `customer_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`customer_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of customer
 -- ----------------------------
-INSERT INTO `customer` VALUES (1, 'Cô Nhẫn', 1, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (1, 'Khách vãng lai', 1, NULL, NULL, NULL);
 INSERT INTO `customer` VALUES (2, 'A Tuấn Dược', 6, NULL, NULL, NULL);
-INSERT INTO `customer` VALUES (6, 'A Tuấn Nhung', 1, NULL, NULL, NULL);
-INSERT INTO `customer` VALUES (7, 'Chị Nhung Tuấn', 1, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (6, 'A Tuấn Nhung', 12, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (7, 'Chị Nhung Tuấn', 12, NULL, NULL, NULL);
 INSERT INTO `customer` VALUES (8, 'Khoa', 8, NULL, NULL, 'Chủ sân Edison');
 INSERT INTO `customer` VALUES (9, 'Thầy Hải', 13, NULL, NULL, NULL);
 INSERT INTO `customer` VALUES (10, 'Hoàng Long', 13, NULL, NULL, NULL);
-INSERT INTO `customer` VALUES (11, 'Đỗ Quân', 12, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (11, 'Đỗ Quân', 1, NULL, NULL, NULL);
 INSERT INTO `customer` VALUES (12, 'A Ngọc', 5, NULL, NULL, 'Chủ tịch Công Viên Ánh Sáng');
 INSERT INTO `customer` VALUES (13, 'A Diện', 5, NULL, NULL, NULL);
 INSERT INTO `customer` VALUES (14, 'A Hoàng Mobile', 5, NULL, NULL, NULL);
-INSERT INTO `customer` VALUES (16, 'A Chu Tiến', 1, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (16, 'A Chu Tiến', 12, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (18, 'Cô Nhẫn', 12, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (19, 'Chị Liên', 6, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (20, 'A Nghĩa vui tính', 12, NULL, NULL, NULL);
+INSERT INTO `customer` VALUES (21, 'Chú Văn', 6, NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for customergroup
@@ -61,7 +65,7 @@ CREATE TABLE `customergroup`  (
 -- ----------------------------
 -- Records of customergroup
 -- ----------------------------
-INSERT INTO `customergroup` VALUES (1, 'Long Hưng - Ve Sầu', NULL);
+INSERT INTO `customergroup` VALUES (1, 'Vãng Lai', NULL);
 INSERT INTO `customergroup` VALUES (2, 'Cửu Cao', NULL);
 INSERT INTO `customergroup` VALUES (3, 'Vinhome Ocean Park 1', NULL);
 INSERT INTO `customergroup` VALUES (4, 'Vinhome Ocean Park 2,3', NULL);
@@ -72,7 +76,7 @@ INSERT INTO `customergroup` VALUES (8, 'Ecopark', NULL);
 INSERT INTO `customergroup` VALUES (9, 'Thị Trấn', NULL);
 INSERT INTO `customergroup` VALUES (10, 'Dương Quảng Hàm', NULL);
 INSERT INTO `customergroup` VALUES (11, 'Lại Ốc', NULL);
-INSERT INTO `customergroup` VALUES (12, 'Vãng Lai', NULL);
+INSERT INTO `customergroup` VALUES (12, 'Long Hưng - Ve Sầu', NULL);
 INSERT INTO `customergroup` VALUES (13, 'Long Hưng - Buổi Sáng', NULL);
 
 -- ----------------------------
@@ -93,7 +97,7 @@ CREATE TABLE `product`  (
   `product_source` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `productype_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`product_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 167 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 168 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of product
@@ -135,6 +139,35 @@ INSERT INTO `product` VALUES (163, 'Vợt Cầu Lông Victor Brave Sword 12', '2
 INSERT INTO `product` VALUES (164, 'Vợt Cầu Lông Victor TK-F Ultra', '3100000', '3400000', NULL, '1', NULL, 'Cái', NULL, NULL, NULL, NULL);
 INSERT INTO `product` VALUES (165, 'Vợt cầu lông Lining Calibar 300B', '1125000', '1600000', NULL, '1', NULL, 'Cái', NULL, NULL, NULL, NULL);
 INSERT INTO `product` VALUES (166, 'Vợt Cầu Lông Lining Calibar 600', '1885091', '2700000', NULL, '1', NULL, 'Cái', NULL, NULL, NULL, NULL);
+INSERT INTO `product` VALUES (167, 'Quấn cán vợt cầu lông VS', '7000', '10000', NULL, '100', NULL, 'Cái', NULL, NULL, NULL, '23');
+
+-- ----------------------------
+-- Table structure for productorder
+-- ----------------------------
+DROP TABLE IF EXISTS `productorder`;
+CREATE TABLE `productorder`  (
+  `productorder_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NULL DEFAULT NULL,
+  `productorder_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `product_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `productorder_status` int(255) NULL DEFAULT NULL,
+  `productorder_timereturn` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `productorder_ispayment` int(11) NULL DEFAULT NULL,
+  `productorder_discount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`productorder_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of productorder
+-- ----------------------------
+INSERT INTO `productorder` VALUES (17, 19, NULL, '15', 1, 'chiều 26/6', 0, NULL, NULL, '2025-06-26 03:07:44');
+INSERT INTO `productorder` VALUES (18, 20, NULL, '25', 2, 'chiều 25/6', 0, NULL, NULL, '2025-06-26 03:07:41');
+INSERT INTO `productorder` VALUES (19, 21, NULL, '21', 0, 'chiều 26/6', 1, NULL, NULL, '2025-06-26 03:08:44');
+INSERT INTO `productorder` VALUES (20, 8, NULL, '13', 1, 'chiều 26/6', 0, '15000', NULL, '2025-06-26 03:08:11');
+INSERT INTO `productorder` VALUES (21, 8, NULL, '4', 0, 'chờ xác nhận', 0, NULL, '2025-06-26 03:09:36', '2025-06-26 03:10:14');
+INSERT INTO `productorder` VALUES (23, 1, NULL, '167', NULL, 'trực tiếp', 1, NULL, '2025-06-26 03:48:32', NULL);
 
 -- ----------------------------
 -- Table structure for productype
@@ -145,7 +178,7 @@ CREATE TABLE `productype`  (
   `productype_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `productype_parent_id` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`productype_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of productype
@@ -153,6 +186,7 @@ CREATE TABLE `productype`  (
 INSERT INTO `productype` VALUES (20, 'Vợt cầu lông', NULL);
 INSERT INTO `productype` VALUES (21, 'Giày cầu lông', NULL);
 INSERT INTO `productype` VALUES (22, 'Quần cầu lông', NULL);
+INSERT INTO `productype` VALUES (23, 'Quấn cán vợt cầu lông', NULL);
 
 -- ----------------------------
 -- Table structure for string
@@ -197,7 +231,7 @@ INSERT INTO `string` VALUES (21, 'Kizuna Z61S', '', '2100000', '150000', '1', '9
 INSERT INTO `string` VALUES (22, 'Kizuna Z63', NULL, '155000', '190000', '2', '0', '0');
 INSERT INTO `string` VALUES (23, 'Kizuna Z66', NULL, '155000', '190000', '2', '0', '1');
 INSERT INTO `string` VALUES (24, 'Kizuna Z69', NULL, '0', '130000', '2', '0', '4');
-INSERT INTO `string` VALUES (25, 'Kizuna Z69 Ti', '', '1830000', '130000', '1', '1', '1');
+INSERT INTO `string` VALUES (25, 'Kizuna Z69 Ti', NULL, '1830000', '140000', '1', '4', '1');
 INSERT INTO `string` VALUES (26, 'Cước học sinh', NULL, '15000', '80000', '2', '0', '10');
 
 -- ----------------------------
@@ -210,18 +244,22 @@ CREATE TABLE `stringorder`  (
   `stringorder_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `string_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `stringorder_kg` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `stringorder_revenue` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `stringorder_status` int(255) NULL DEFAULT NULL,
   `stringorder_timereturn` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `stringorder_ispayment` int(11) NULL DEFAULT NULL,
   `stringorder_discount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`stringorder_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of stringorder
 -- ----------------------------
-INSERT INTO `stringorder` VALUES (11, 14, 'Ex dolores eaque nul', '15', '43', '-25', 2, 'Repudiandae exercita', 0, '82');
-INSERT INTO `stringorder` VALUES (12, 1, 'Eius quas non sint i', '2', '69', '50', 0, 'Ipsum saepe est se', 0, '10');
+INSERT INTO `stringorder` VALUES (17, 19, NULL, '15', '10.5', 1, 'chiều 26/6', 0, NULL, NULL, '2025-06-26 03:07:44');
+INSERT INTO `stringorder` VALUES (18, 20, NULL, '25', '11', 2, 'chiều 25/6', 0, NULL, NULL, '2025-06-26 03:07:41');
+INSERT INTO `stringorder` VALUES (19, 21, NULL, '21', '10.6', 0, 'chiều 26/6', 1, NULL, NULL, '2025-06-26 03:08:44');
+INSERT INTO `stringorder` VALUES (20, 8, NULL, '13', '11.5', 1, 'chiều 26/6', 0, '15000', NULL, '2025-06-26 03:08:11');
+INSERT INTO `stringorder` VALUES (21, 8, NULL, '4', NULL, 0, 'chờ xác nhận', 0, NULL, '2025-06-26 03:09:36', '2025-06-26 03:10:14');
 
 SET FOREIGN_KEY_CHECKS = 1;
