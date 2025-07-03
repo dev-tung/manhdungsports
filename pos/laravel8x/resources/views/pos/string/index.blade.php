136460dc49ec8facc4443279d162bf0938fcc847
Tung <devtung27101992@gmail.com>
2025-07-03 11:36:41 +0700

    update code

--- pos/laravel8x/resources/views/pos/string/index.blade.php
+++ pos/laravel8x/resources/views/pos/string/index.blade.php
@@ -7,9 +7,9 @@
                 <form action="{{route('string.index', ['screen'=>'pos'])}}" class="ListSearchForm">
                     <input class="ListSearchFormInput" type="text" name="string_name" placeholder="Tìm kiếm ..." value="{{ request()->string_name }}">
 
-                    <button class="ListSearchFormSubmit">
+                    <button class="ListSearchFormBtn ListSearchFormBtn_Submit">
                         <svg class="ListSearchFormSubmitIcon w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
                         </svg>
                     </button>
                 </form>
