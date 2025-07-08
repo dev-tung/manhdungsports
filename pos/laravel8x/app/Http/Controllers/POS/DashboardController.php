e42ae7da3be0f6b63686812980572ebd5c59a2ec
Tung <devtung27101992@gmail.com>
2025-07-08 21:09:44 +0700

    update code

--- pos/laravel8x/app/Http/Controllers/POS/DashboardController.php
+++ pos/laravel8x/app/Http/Controllers/POS/DashboardController.php
@@ -6,27 +6,30 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Access\InvoiceAccess;
 use App\Access\ExpenseAccess;
+use App\Access\ProductAccess;
 
 class DashboardController extends Controller
 {
     function __construct() {
         $this->_invoiceAccess = new InvoiceAccess();
         $this->_expenseAccess = new ExpenseAccess();
+        $this->_productAccess = new ProductAccess();
     }
 
     public function index(Request $request){
         $todayInvoiceMoney = $this->_invoiceAccess->todayMoney();
         $todayExpenseMoney = $this->_expenseAccess->todayMoney();
+        $priceTotalInput = $this->_productAccess->priceTotalInput();
 
         $todayTotalRevenue = array_sum(array_column($todayInvoiceMoney, 'invoice_revenue'));
         $todayExpenseMoney = array_sum(array_column($todayExpenseMoney, 'expense_money'));
         $todayTotalProfit = array_sum(array_column($todayInvoiceMoney, 'invoice_profit')) - $todayExpenseMoney;
-        
 
         return view($request->screen.'.dashboard.index', [
             'todayTotalRevenue' => commonNumberToVND($todayTotalRevenue),
             'todayTotalProfit' => commonNumberToVND($todayTotalProfit),
-            'todayExpenseMoney' => commonNumberToVND($todayExpenseMoney)
+            'todayExpenseMoney' => commonNumberToVND($todayExpenseMoney),
+            'priceTotalInput' => commonNumberToVND($priceTotalInput)
         ]);
     }
 }
--- pos/laravel8x/resources/views/pos/dashboard/index.blade.php
+++ pos/laravel8x/resources/views/pos/dashboard/index.blade.php
@@ -4,6 +4,10 @@
         <main class="Main">
             <div class="CardGrid">
                 <div class="CardGridItem Bg_Primary">
+                    <h3 class="CardTitle">Tồn kho</h3>
+                    <p class="CardValue">{{ $priceTotalInput }}</p>
+                </div>
+                <div class="CardGridItem Bg_Success">
                     <h3 class="CardTitle">Doanh thu hôm nay</h3>
                     <p class="CardValue">{{ $todayTotalRevenue }}</p>
                 </div>
