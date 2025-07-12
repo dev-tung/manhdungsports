8f7a2bb1859699bb5bd9c86ec01c5c183f81fe29
Tung <devtung27101992@gmail.com>
2025-07-13 04:02:49 +0700

    update code

--- pos/laravel8x/public/pos/js/invoice/add.js
+++ pos/laravel8x/public/pos/js/invoice/add.js
@@ -38,41 +38,49 @@ Validator({
 });
 // END VALIDATOR
 
-let _b = document.querySelector('input[data-modal-action="toggle"]');
-let _e = document.querySelector(_b.getAttribute('data-modal-target'));
 
-let customerSearchModal = new Promise((resolve, reject) => {
-    document.getElementById('CustomerSearch').style.display = "none";
-    document.getElementById("ModalLoading").style.display = "block";
+// CUSTOMER TOGGLE SEARCH
+let _customerToggleInput = document.querySelector('input[data-modal-action="toggle"]');
+let _customerSearchBox = document.querySelector(_customerToggleInput.getAttribute('data-modal-target'));
 
-    _b.addEventListener("click", function(){
-        _e.classList.add("Show");
-        document.getElementById('CustomerSearchFormInput').focus();
-    });
-    
-    _e.addEventListener('click', function(event){
-        if (_e !== event.target) return;
-        _e.classList.remove("Show");
-    }, false); 
+let customerToggleSearchInit = new Promise((success, error) => {
+    try {
+        document.getElementById('CustomerSearch').style.display = "none";
+        document.getElementById("ModalLoading").style.display = "block";
 
-    document.getElementById("CustomerSearchForm_Reset").addEventListener("click", function(){
-        document.getElementById("CustomerSearchFormInput").value = "";
-        document.getElementById('CustomerSearchResult').innerHTML = "";
-    });
+        _customerToggleInput.addEventListener("click", function(){
+            _customerSearchBox.classList.add("Show");
+            document.getElementById('CustomerSearchFormInput').focus();
+        });
+        
+        _customerSearchBox.addEventListener('click', function(event){
+            if (_customerSearchBox !== event.target) return;
+            _customerSearchBox.classList.remove("Show");
+        }, false); 
 
-    document.onkeyup = function(press) {
-        if(press.key === "Escape") {
-            _e.classList.remove("Show");
+        document.getElementById("CustomerSearchForm_Reset").addEventListener("click", function(){
+            document.getElementById("CustomerSearchFormInput").value = "";
+            document.getElementById('CustomerSearchResult').innerHTML = "";
+        });
+
+        document.onkeyup = function(press) {
+            if(press.key === "Escape") {
+                _customerSearchBox.classList.remove("Show");
+            }
         }
+        
+        success(true);
     }
-
-    resolve(true);
+    catch(err) {
+        error('customerToggleSearchInit Fail!');
+    }
+    
 });
 
-customerSearchModal.then(response => {
-    if( response ){
+let customerToggleSearchFetch = new Promise((success, error) => {
+    try {
         let apiCustomerGet = document.querySelector("#apiCustomerGet").content;
-        fetch(apiCustomerGet,{
+        fetch(apiCustomerGet, {
             method: 'GET', 
             headers: {
                 'Content-Type': 'application/json',
@@ -81,63 +89,68 @@ customerSearchModal.then(response => {
             }
         })
         .then(response => {
-            return response.json();
+            document.getElementById('CustomerSearch').style.display = "block";
+            document.getElementById("ModalLoading").style.display = "none";
+            success(response.json());
         })
-        .then(response => {
-            if( response.customers ){
-                document.getElementById('CustomerSearch').style.display = "block";
-                document.getElementById("ModalLoading").style.display = "none";
-                const searchInput = document.getElementById('CustomerSearchFormInput');
-
-                searchInput.addEventListener("keyup", () => {
-                    let fetchCustomerDone = new Promise((resolve, reject) => {
-                        let CustomerSearchResult = document.getElementById('CustomerSearchResult');
-                        let SearchValue = searchInput.value;
-
-                        if( SearchValue == '' ){
-                            CustomerSearchResult.innerHTML = `<p class="CustomerSearchResult_No">No recent searches</p>`;
-                            return;
-                        }
-
-                        function GetSearchItem(Content, SearchValue, Item){
-                            Content = Content.replace(new RegExp(SearchValue, 'gi'), '<mark>$&</mark>');
-                            return `<div class="CustomerSearchItem" data-customer_id="${Item.customer_id}" data-customer_name="${Item.customer_name}">
-                                        <div class="CustomerSearchContent">
-                                            <p class="CustomerSearchContent-Desc">${Content}</p>
-                                        </div>
-                                    </div>`;
-                        } 
-
-                        CustomerSearchResult.innerHTML = '';
-                        response.customers.forEach(Item => {
-                            let Content = Item.customer_name + ' - ' + Item.customergroup_name;
-                            if( Content.toUpperCase().indexOf(SearchValue.toUpperCase()) != -1 ){
-                                CustomerSearchResult.innerHTML += GetSearchItem(Content, SearchValue, Item);
-                            }
-                        });
-
-                        resolve(true);
-                    });
-
-                    fetchCustomerDone.then( response =>  {
-                        document.querySelectorAll('.CustomerSearchItem').forEach(item => {
-                            item.addEventListener("click", function(){
-                                document.getElementById('CustomerId').value = item.getAttribute('data-customer_id');
-                                let customerName = document.getElementById('CustomerName');
-                                customerName.value = item.getAttribute('data-customer_name');
-                                customerName.focus();
-                                customerName.blur();
-                                _e.classList.remove("Show");
-                            });
-                        })
-                    })
-                });
-                // END KEYUP EVENT
-
-            }
-        });
     }
-
+    catch(err) {
+        error('customerToggleSearchFetch Fail!');
+    }
 });
 
+let customerToggleSearchKeyup = function(object){
+    return new Promise((success, error) => {
+        try {
+            const searchInput = document.getElementById('CustomerSearchFormInput');
+            searchInput.addEventListener("keyup", () => {
+                let CustomerSearchResult = document.getElementById('CustomerSearchResult');
+                let SearchValue = searchInput.value;
 
+                if( SearchValue == '' ){
+                    CustomerSearchResult.innerHTML = `<p class="CustomerSearchResult_No">No recent searches</p>`;
+                    return;
+                }
+
+                CustomerSearchResult.innerHTML = '';
+                object.customers.forEach(Item => {
+                    let Content = Item.customer_name + ' - ' + Item.customergroup_name;
+                    if( Content.toUpperCase().indexOf(SearchValue.toUpperCase()) != -1 ){
+                        Content = Content.replace(new RegExp(SearchValue, 'gi'), '<mark>$&</mark>');
+                        CustomerSearchResult.innerHTML += 
+                        `<div class="CustomerSearchItem" data-customer_id="${Item.customer_id}" data-customer_name="${Item.customer_name}">
+                            <div class="CustomerSearchContent">
+                                <p class="CustomerSearchContent-Desc">${Content}</p>
+                            </div>
+                        </div>`;
+                    }
+                });
+
+                document.querySelectorAll('.CustomerSearchItem').forEach(item => {
+                    item.addEventListener("click", function(){
+                        document.getElementById('CustomerId').value = item.getAttribute('data-customer_id');
+                        let customerName = document.getElementById('CustomerName');
+                        customerName.value = item.getAttribute('data-customer_name');
+                        customerName.focus();
+                        customerName.blur();
+                        _customerSearchBox.classList.remove("Show");
+                    });
+                })
+            });
+        }
+        catch(err) {
+            error('customerToggleSearchKeyup Fail!');
+        }
+    });
+}
+
+customerToggleSearchInit
+.then( response => {
+    return customerToggleSearchFetch;
+})
+.then( response => {
+    customerToggleSearchKeyup(response);
+})
+.catch( response => console.log(response) )
+
+// END CUSTOMER TOGGLE SEARCH
