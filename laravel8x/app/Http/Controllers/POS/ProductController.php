df9e6849e3a6b4ee8da81d88bb4b31a27d549bd7
Tung <devtung27101992@gmail.com>
2025-07-22 04:07:16 +0700

    replace class name web

--- laravel8x/app/Http/Controllers/POS/ProductController.php
+++ laravel8x/app/Http/Controllers/POS/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
     function __construct() {
         $this->_productAccess = new ProductAccess();
         $this->_productypeAccess = new ProductypeAccess();
-        $this->_productsevice = new ProductService();
+        $this->_productSevice = new ProductService();
     }
 
     public function index(Request $request){
@@ -41,7 +41,7 @@ class ProductController extends Controller
     }
 
     public function insert(Request $request){
-        $this->_productsevice->moveThumbnail($request);
+        $this->_productSevice->moveThumbnail($request);
         $this->_productAccess->insert($request);
         return redirect()->route('product.add', ['screen'=>'pos']);
     }
@@ -53,14 +53,14 @@ class ProductController extends Controller
     }
 
     public function update(Request $request){
-        $this->_productsevice->moveThumbnail($request);
+        $this->_productSevice->moveThumbnail($request);
         $this->_productAccess->update($request);
         return redirect()->route('product.index', ['screen'=>'pos']);
     }
 
     public function delete(Request $request){
         $product = $this->_productAccess->getFirst(['product_id' => $request->product_id]);
-        $this->_productsevice->deleteThumbnail($request, $product);
+        $this->_productSevice->deleteThumbnail($request, $product);
         $this->_productAccess->delete(['product_id' => $request->product_id]);
         return redirect()->route('product.index', ['screen'=>'pos']);
     }
