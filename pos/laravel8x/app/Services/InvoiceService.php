d207f6469a828e568fa9daa620810c3165d689d6
Tung <devtung27101992@gmail.com>
2025-07-24 11:25:33 +0700

    update code

--- pos/laravel8x/app/Services/InvoiceService.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services;
-use DB;
-
-class InvoiceService extends Service{
-
-}
--- pos/laravel8x/resources/views/mpos/dashboard/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('pos.layouts.cover')
-@section('Title', 'MANH DUNG POS')
-@section('Main')
-        <main class="Main">
-            Dashboard
-        </main>
-        <!-- End Main -->
-@endsection
